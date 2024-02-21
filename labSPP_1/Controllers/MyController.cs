1b25624b494acb85da2a713fd2288c1c90ea853d
Egor Zaprigaev <egorzap77@gmail.com>
2024-02-21 23:13:00 +0300

    Добавьте файлы проекта.

---
+++ labSPP_1/Controllers/MyController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace labSPP_1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MyController : ControllerBase
+    {
+        [HttpGet("task1")]
+        public string GetString()
+        {
+            return "Сообщение";
+        }
+    }
+}
